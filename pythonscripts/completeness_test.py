1609467bf8bb68daabda28d111cd38c27a98f7a8
22nicolet <67885081+22nicolet@users.noreply.github.com>
2020-08-01 11:15:33 -0700

    Update completeness_test.py

--- pythonscripts/completeness_test.py
+++ pythonscripts/completeness_test.py
@@ -1,140 +1,155 @@
-from   astropy.io import fits
+#adds artificial stars to the original image
+from astropy.io import fits
+import config as cfg
 import copy
 import csv
-import config as cfg
-from astropy.table import Table
 import matplotlib.pyplot as plt
-from   matplotlib.colors import LogNorm
 import numpy as np
-#from pydl.pydlutils.spheregroup import spherematch
 import os,sys
-import statistics
 import time
 
-# generates command to run SExtractor
-def run_sextractor(catalog_file, image_file1, image_file2, config_file, weight_file1, weight_file2):
-    # run sextractor to generate catalog
-    command = "sex " + image_file1 + "," + image_file2 + " -c " + config_file + " -WEIGHT_IMAGE " + weight_file1 + "," + weight_file2
-    print("running command " + command)
-    os.system(command)
+# @precondition psf data is from a psf file, sigma_array has no negatives
+# adds gaussian noise (mean = pixel value, sigma = sqrt(pixel value/gain)) to each pixel of the psf
+def add_noise_to_psf(filter_index):
+    mean_array = np.copy(psf_data_array[filter_index])
+    sigma_array = np.copy(psf_data_array[filter_index])
+    sigma_array = np.sqrt(sigma_array / int(gain))
+    psf_mag_noise = np.random.normal(mean_array, sigma_array)
 
-    # rename the catalog
-    command = "mv output.fits " + catalog_file
-    os.system(command)
+    #print(psf_data[filter_index])
+    #plt.figure(figsize=[5,5])
+    #plt.imshow(psf_data[filter_index],origin='lower',cmap='gray_r')
+    #plt.show()
+    #print(psf_data)
+    #plt.figure(figsize=[5,5])
+    #plt.imshow(psf_mag_noise[filter_index],origin='lower',cmap='gray_r')
+    #plt.show()
+    #print(psf_mag_noise)
 
-# precondition : config and parameter files are copied to where the python script is run.
-# runs SExtractor for all original galaxies and images with artificial stars to generate catalog files
-def run_sextractor_for_all():
-    # Loop on all galaxies
+    return psf_mag_noise
+
+# adds in NUM_GRID_POINTS * NUM_GRID_POINTS stars into the image, stars are added to a square grid around the center of the PSF
+def add_star_one_step(orig_image, image_data_origin, grid_space, x_step_size, y_step_size, ratio, mag, suffix, galaxy_name, filter_index, filter_name, noise_index, psf_data_noise):
+    image_data_addstar = copy.deepcopy(image_data_origin)
+    info_added_stars = []
+
+    for i in range (0, cfg.NUM_GRID_POINTS):
+        for j in range (0, cfg.NUM_GRID_POINTS):
+            position_x = int(grid_space * (i + 1) + x_step_size)
+            position_y = int(grid_space * (j + 1) + y_step_size)
+            info_added_stars.append([position_x, position_y, mag])
+
+            # PSF image is 31x31 pixels, calculate x_min/max, y_min/max so PSF's center is at position_x and position_y
+            x_min = position_x - 15
+            x_max = position_x + 16
+            y_min = position_y - 15
+            y_max = position_y + 16
+
+            # Overlay the fake star on top of the image background
+            image_data_addstar[y_min:y_max, x_min:x_max] = image_data_addstar[y_min:y_max, x_min:x_max] + psf_data_noise / ratio
+
+    #plt.figure(figsize=[8, 8])
+    #plt.imshow(image_data_addstar, origin='lower', cmap='gray_r', norm=LogNorm())
+    #plt.title('After add stars on the image', fontsize=14)
+    #plt.show()
+
+    # Write out image with added stars
+    new_image_file = result_dir_name + "/" + galaxy_name + "/" + galaxy_name + "_" + filter_name + "_addstar" + str(mag) + "_" + suffix + "_" + str(noise_index) + ".fits"
+    hdu = fits.PrimaryHDU(image_data_addstar, orig_image[0].header)
+    hdu.writeto(new_image_file, overwrite=True)
+
+    truth_file = result_dir_name + "/" + galaxy_name + "/" + galaxy_name + "_" + filter_name + "_addstar" + str(mag) + "_" + suffix + "_" + str(noise_index) + "_truth.csv"
+    with open(truth_file, 'w') as csvout:
+        csv_writer = csv.writer(csvout)
+        for star in info_added_stars:
+            csv_writer.writerow(star)
+
+# nested loop to add stars into images (galaxy, filter, number of different noises, magnitudes)
+# each magnitude bin has four steps spaced by half the grid_space (size of the image/(NUM_GRID_POINTS + 1)), the steps form a square (shifted left, right, diagonally)
+def add_stars():
     for g in galaxy_names:
-        # Run sextractor for original images
+        base_dir_name = image_dir_name + "/" + g
         base_file_name = image_dir_name + "/" + g + "/" + g + "_"
-        image_file1 = base_file_name + "g_" + image_file_suffix + ".fits"           # Always use green filter image for source detection
-        weight_file1 = weight_dir_name + "/" + g + "/" + g + "_" + "g_sig.fits"     # Weight file of green filter image
-        for f in filter_names:
-            image_file2 = base_file_name + f + "_" + image_file_suffix + ".fits"    # File for measurement
-            weight_file2 = weight_dir_name + "/" + g + "/" + g + "_" + f + "_sig.fits"        # Corresponding weight file
-            catalog_file =  g + "_" + f + "_" + image_file_suffix + "_cat.fits"
-            run_sextractor(catalog_file, image_file1, image_file2, config_file_name, weight_file1, weight_file2)
-
-            tmp_dir_name = result_dir_name + "/" + g + "/"
-            if not os.path.exists(tmp_dir_name):
-                os.mkdir(tmp_dir_name)
-            command = "mv " + catalog_file + " " + tmp_dir_name
-            os.system(command)
-
-        # Run sextractor for images with added stars
-        base_file_name = result_dir_name + "/" + g + "/" + g + "_"
-        for n in range(0, cfg.NUMBER_NOISES):
-            for mag in magnitudes:
-                for suffix in range(0, num_suffix):
-                    image_file1 = base_file_name + "g_addstar" + str(mag) + "_" + str(suffix) + "_" + str(n) + ".fits"  # Always use green filter image for source detection
-                    weight_file1 = weight_dir_name + "/" + g + "/" + g + "_" + "g_sig.fits"                 # Weight file of green filter image
-                    for f in filter_names:
-                        image_file2 = base_file_name + f + "_addstar" + str(mag) + "_" + str(suffix) + "_" + str(n) + ".fits"
-                        weight_file2 = weight_dir_name + "/" + g + "/" + g + "_" + f + "_sig.fits"
-
-                        catalog_file =  g + "_" + f + "_addstar" + str(mag) + "_" + str(suffix)  + "_" + str(n) + "_cat.fits"
-                        run_sextractor(catalog_file, image_file1, image_file2, config_file_name, weight_file1, weight_file2)
-
-                        tmp_dir_name = result_dir_name + "/" + g + "/"
-                        if not os.path.exists(tmp_dir_name):
-                            os.mkdir(tmp_dir_name)
-                        command = "mv " + catalog_file + " " + tmp_dir_name
-                        os.system(command)
-
-# plots the completeness function
-def plot_completeness_function(x_data, y_data):
-    color = "black"
-    plt.figure(1)
-    plt.scatter(x=x_data, y=y_data, c='r', marker='x', label='GC detection ratio', alpha=0.5)
-    plt.ylim(0.0, 1.0)
-    plt.legend()
-    plt.title('Completeness function (NUM_GRID_POINTS = ' + str(cfg.NUM_GRID_POINTS) + ')', fontsize=14)
-    plt.xlabel("Magnitudes")
-    plt.ylabel("Detection ratio")
-    plt.show()
-
-# finds detection ratio:
-# number of artificial stars detected = number of stars detected in the images with added psf - number detected in the original image
-# average the difference among the four steps in each magnitude
-# divide difference by total number of artificial stars added to get detection ratio
-def completeness_test():
-    for g in galaxy_names:
         filter_index = 0
         for f in filter_names:
-            num_star_per_mag_bin = [0 for x in range(len(magnitudes))]
-
-            # Get number of detected sources from sextractor produced catalog of original image
-            orig_fits_file = result_dir_name + "/" + g + "/" + g + "_" + f + "_modsub2_cat.fits"
-            orig_cat = fits.open(orig_fits_file)
-            #orig_cat.info()
-            orig_cat_data = Table(orig_cat[2].data)
-            orig_num_detected = len(orig_cat_data)
-
+            orig_image_file = base_file_name + f + "_modsub2.fits"  # File for measurement
+            orig_image = fits.open(orig_image_file)
+            image_data_origin = orig_image[0].data
+            size = len(image_data_origin[1])                        # size is the dimension of the square image
+            grid_space = size/(cfg.NUM_GRID_POINTS + 1)                 # the number of pixels between two grid points
             for n in range(0, cfg.NUMBER_NOISES):
-                # Get number of detected sources from sextractor produced catalog of image with added stars
-                mag_index = 0
+                # Add Gaussian noises to each pixel of the PSF
+                psf_data_noise = add_noise_to_psf(filter_index)
 
-                for mag in magnitudes:
-                    for suffix in range(0, num_suffix):
-                        tmp_dir_name = result_dir_name + "/" + g + "/"
-                        catalog_file = tmp_dir_name + g + "_" + f + "_addstar" + str(mag) + "_" + str(suffix) + "_" + str(n) + "_cat.fits"
-                        addstar_cat = fits.open(catalog_file)
-                        #addstar_cat.info()
-                        addstar_cat_data = Table(addstar_cat[2].data)
-                        num_star_per_mag_bin[mag_index] += len(addstar_cat_data) - orig_num_detected
+                # Scale to some magnitude
+                # Equations: m1-m2=-2.5log(F1/F2), F1/F2=10**(-0.4*(m1-m2))
+                psf_mag = -2.5 * np.log10(sum(sum(psf_data_noise))) + 30
+                print('galaxy='+g+' filter='+f+' noise_idx='+str(n)+' psf_mag='+str(psf_mag))
 
-                    # Average numbers for all catalogs per magnitude bin, divided by true number of added stars
-                    num_star_per_mag_bin[mag_index] /= (1.0 * num_suffix * (cfg.NUM_GRID_POINTS*cfg.NUM_GRID_POINTS))
-                    mag_index += 1
+                for mag in magnitude_ranges:  # generate fake stars of different magnitude
+                    ratio = 10 ** (-0.4 * (psf_mag - mag))  # calculate the flux ratio
 
-            filter_index += 1
-            plot_completeness_function(magnitudes, num_star_per_mag_bin)
+                    add_star_one_step(orig_image, image_data_origin, grid_space, 0, 0, ratio, mag, "0", g, filter_index, f, n, psf_data_noise)
+                    add_star_one_step(orig_image, image_data_origin, grid_space, grid_space/2, 0, ratio, mag, "1", g, filter_index, f, n, psf_data_noise)
+                    add_star_one_step(orig_image, image_data_origin, grid_space, 0, grid_space/2, ratio, mag, "2", g, filter_index, f, n, psf_data_noise)
+                    add_star_one_step(orig_image, image_data_origin, grid_space, grid_space/2,  grid_space/2, ratio, mag, "3", g, filter_index, f, n, psf_data_noise)
 
-# main
+        #            plt.figure(figsize=[8, 8])
+        #            plt.imshow(image_data_origin, origin='lower', cmap='gray_r', norm=LogNorm())
+        #            plt.title('Galaxy model subtracted image', fontsize=14)
+        #            plt.show()
+
+        #            plt.figure(figsize=[8, 8])
+        #            plt.imshow(image_data_addstar, origin='lower', cmap='gray_r', norm=LogNorm())
+        #            plt.title('After add stars on the image', fontsize=14)
+        #            plt.show()
+
+
+#main
 filter_names = ("g")
-image_dir_name = "../images"
-result_dir_name = "../results"
-config_file_name = "ngvs.sex"
+image_dir_name = "../../../SIP2020/images"
+result_dir_name = "../../../SIP2020/results"
 galaxy_names = ["VCC0940"]
-magnitudes = (23, 23.5, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28)
+magnitude_ranges = (23, 23.5, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28)
 NUM_STARS_TO_ADD_PER_MAG = 100
-INDEX_4 = 5
-INDEX_8 = 9
-num_suffix = 4
 
+# Downloading PSF files
+psf_data = [0 for x in range(len(filter_names))]
+psf_data_array = [0 for x in range(len(filter_names))]
 filter_index = 0
 
-weight_dir_name = "../images"
-image_file_suffix = "modsub2"
+# Load effective gain table from csv file from
+# http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/en/community/ngvs/docs/gain.html
+gain = 1
+gain_table = {}
+with open('ngvs_gain_table.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        gain_table[row[0]] = row[1]
 
-if len(sys.argv) == 3:
-    image_dir_name = sys.argv[1]
-    image_file_suffix = sys.argv[2]
 
+for f in filter_names:
+    image_key = 'NGVS-1+0.l.' + f
+    image = 'image=' + image_key + '.Mg002.fits'   # You may change the fieldname, band, x&y position etc.
+    x = '17366'
+    y = '19409'
+    psf_URL = '\'' +'https://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/cadcbin/community/ngvs/NGVSpsf.pl?'+image+'&x='+x+'&y='+y + '\''
+    print(psf_URL)
+    os.system('wget -O psf.fits %s' %psf_URL)           # Use wget command to download the file
+    time.sleep(1)                                       # Give wget some time to get the file from web
+
+    #reads in psf, changes the negative data numbers to 0, stores in a numpy array for further processing
+    hdulist = fits.open('psf.fits')
+    psf_data[filter_index] = hdulist[0].data
+    np_array = np.array(psf_data[filter_index]) # Convert list into a numpy array
+    np_array_sign = np.sign(np_array)  # negative elements become -1, 0 stays 0, positive elements become 1
+    np_array *= (np_array > 0)                  # converts negative elements to -0
+    np_array *= np_array_sign                   # replaces -0 with 0 to prevent error later on
+    psf_data_array[filter_index] = np.copy(np_array)
+    filter_index += 1
+
+gain = gain_table[image_key]
 start = time.time()
-run_sextractor_for_all()
+add_stars()
 end = time.time()
 print('Elapsed time = '+str(end-start)+' seconds')
-completeness_test()
